96eb927659de8247c019f0f92753a7d4395d7477
Loganwebb1994 <loganwebb1994@gmail.com>
2021-03-24 15:04:22 -0500

    changed edit button to be singular

--- src/components/stands/StandCard.js
+++ src/components/stands/StandCard.js
@@ -35,13 +35,12 @@ export const StandCard = ({ stand, relationships }) => {
     return(
       <>
       {console.log(userStandId)}
-      <button className="stand__notes__edit" onClick={() => {history.push(`/stands/${stand.id}`)}} id= {stand.id}>Edit Notes</button>
+      <button className="stand__notes__edit" onClick={() => {history.push(`/stands/${stand.id}`)}} id= {stand.id}>Edit Note</button>
       <button className="stand__notes__delete" onClick={() => addNote({note:""}, userStandId )}>Delete Note</button>
-      <button className="stand__addNote" onClick={() => { history.push(`/stands/${stand.id}`) }}>Make Note</button>
+      <button className="stand__addNote" onClick={() => { history.push(`/stands/${stand.id}`) }}>Add Note</button>
       </>
     )
 
- 
   }
   
   return (
@@ -52,7 +51,6 @@ export const StandCard = ({ stand, relationships }) => {
       <div className="buttonContainer">
         {stand.availability === false ? renderNoteButtons() : ""}
       </div>
-        {/* <button className="stand__delete">Delete</button> */}
       <div className="buttonContainer">
         {stand.availability === true ? (<button className="stand__reserve" onClick={handleReserve}>Check In</button>) : ""}
         {stand.availability === false ? (<button className="stand__checkOut" onClick={() => { resetAvailability(stand.id) }}>Check Out</button>) : ""}
